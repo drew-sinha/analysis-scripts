8ead8e9f01ee3e68c2b2b22c4c51966fadeb6e75
DS <indiaman04@gmail.com>
2016-08-16 01:00:27 -0500

    (20160805_...) Adjusted calculation of dying health to be absolute time instead of z-scores; still having some issues with off by one on last alive timepoint based on gghost_age vs. health (patch by using and to get last timepoint alive - ultimately based on health)

--- 20160815_spe9DeathInfoOutput.py
+++ 20160815_spe9DeathInfoOutput.py
@@ -1,5 +1,6 @@
 import analyzeHealth
 import pickle
+import csv
 
 
 import collections
@@ -51,6 +52,14 @@ md_list = [
 
 ann_timestamped_data = annotation_file.compile_expt_timestamped_data(expt_dirs,md_list)
 ann_raw_data = annotation_file.compile_expt_raw_data(expt_dirs)
+ann_timepointed_data = annotation_file.compile_expt_timestamped_data(expt_dirs,md_list, as_timepoints=True)
+
+def save_dict_as_tsv(my_dict, output_file):
+    with open(output_file,'w') as output_fp:
+        output_writer = csv.writer(output_fp,delimiter='\t')
+        output_writer.writerow([a_tag for a_tag in my_dict.keys()])
+        for worm_data in zip(*(my_dict.values())):
+            output_writer.writerow(worm_data)
 
 
 # Load df
@@ -69,11 +78,14 @@ print(strain+": n = {}".format(len(strain_df.worms)))
 
 abb_data = collections.OrderedDict()
 abb_data['Worm'] = ann_timestamped_data['Worm_FullName']
-abb_data['AnnotatedDeathFrame'] = ann_raw_data['Death']
-#abb_data['AnnotatedDeathTimepoint'] = ....
+abb_data['AnnotatedDeathFrame'] = [death_value[1:] if death_value[0].isalpha() else death_value for death_value in ann_raw_data['Death']]   # Specify to use WZ's file
+abb_data['AnnotatedDeathTimepoint'] = ann_timepointed_data['Death']
 abb_data['AnnotatedLifespan'] = (ann_timestamped_data['Death']-ann_timestamped_data['Hatch'])/(3600*24) # Days
-abb_data['InterpolatedLifespan'] = analyzeHealth.selectData.get_adultspans(strain_df)
+abb_data['InterpolatedLifespan'] = analyzeHealth.selectData.get_lifespans(strain_df)/24
 abb_data['Dying Health'] = []
-for worm_data in strain_df.mloc(measures=['ghost_age','health']):
-    last_alive = worm_data[0][np.nonzero(~np.isnan(worm_data[0]))[0][-1]]
-    abb_data['Dying Health'].append(worm_data[1][last_alive])
+health_idx=-4
+for i,worm_data in enumerate(strain_df.mloc(measures=['ghost_age','health'])):
+    last_alive = np.nonzero((~np.isnan(worm_data[0])) & (~np.isnan(worm_data[1])))[0][-1]
+    abb_data['Dying Health'].append(strain_df.display_variables(worm_data[1][last_alive],'health')[0])
+
+save_dict_as_tsv(abb_data, '/media/Data/Work/ZPLab/temp_Emily.tsv')
