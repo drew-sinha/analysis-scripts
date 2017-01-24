bf6b616acffeba8ae03e895c919359dfd27f1aad
DS <>
2017-01-24 13:08:26 -0600

    (wormEpi) Incremental change in directory mapping

--- 20160803_wormEpidemiology.py
+++ 20160803_wormEpidemiology.py
@@ -9,35 +9,35 @@ def get_lsdata(expt_dirs,md_list=None):
     
 
 expt_dirs = [
-    '/mnt/iscopearray/Zhang_William/2016.02.16 spe-9 Run 9/',      #0
-    '/mnt/iscopearray/Zhang_William/2016.02.20 spe-9 Run 10A/',    #1
-    '/mnt/iscopearray/Zhang_William/2016.02.20 spe-9 Run 10B/',    #2
-    '/mnt/iscopearray/Zhang_William/2016.02.26 spe-9 Run 11A/',    #3
-    '/mnt/iscopearray/Zhang_William/2016.02.26 spe-9 Run 11B/',    #4
-    '/mnt/iscopearray/Zhang_William/2016.02.26 spe-9 Run 11C/',    #5
-    '/mnt/iscopearray/Zhang_William/2016.02.26 spe-9 Run 11D/',    #6
-    '/mnt/iscopearray/Zhang_William/2016.02.29 spe-9 Run 12A/',    #7
-    '/mnt/iscopearray/Zhang_William/2016.02.29 spe-9 Run 12B/',    #8
-    '/mnt/iscopearray/Zhang_William/2016.03.04 spe-9 Run 13A/',    #9
-    '/mnt/iscopearray/Zhang_William/2016.03.04 spe-9 Run 13B/',    #10
-    '/mnt/iscopearray/Zhang_William/2016.03.04 spe-9 Run 13C/',    #11
-    '/mnt/iscopearray/Zhang_William/2016.03.14 spe-9 Run 14/',     #12
-    '/mnt/iscopearray/Zhang_William/2016.03.25 spe-9 Run 15A/',    #13
-    '/mnt/iscopearray/Zhang_William/2016.03.25 spe-9 Run 15B/',    #14
-    '/mnt/iscopearray/Zhang_William/2016.03.31 spe-9 Run 16/']     #15
+    '/mnt/iscopearray/zhangw/2016.02.16 spe-9 Run 9/',      #0
+    '/mnt/iscopearray/zhangw/2016.02.20 spe-9 Run 10A/',    #1
+    '/mnt/iscopearray/zhangw/2016.02.20 spe-9 Run 10B/',    #2
+    '/mnt/iscopearray/zhangw/2016.02.26 spe-9 Run 11A/',    #3
+    '/mnt/iscopearray/zhangw/2016.02.26 spe-9 Run 11B/',    #4
+    '/mnt/iscopearray/zhangw/2016.02.26 spe-9 Run 11C/',    #5
+    '/mnt/iscopearray/zhangw/2016.02.26 spe-9 Run 11D/',    #6
+    '/mnt/iscopearray/zhangw/2016.02.29 spe-9 Run 12A/',    #7
+    '/mnt/iscopearray/zhangw/2016.02.29 spe-9 Run 12B/',    #8
+    '/mnt/iscopearray/zhangw/2016.03.04 spe-9 Run 13A/',    #9
+    '/mnt/iscopearray/zhangw/2016.03.04 spe-9 Run 13B/',    #10
+    '/mnt/iscopearray/zhangw/2016.03.04 spe-9 Run 13C/',    #11
+    '/mnt/iscopearray/zhangw/2016.03.14 spe-9 Run 14/',     #12
+    '/mnt/iscopearray/zhangw/2016.03.25 spe-9 Run 15A/',    #13
+    '/mnt/iscopearray/zhangw/2016.03.25 spe-9 Run 15B/',    #14
+    '/mnt/iscopearray/zhangw/2016.03.31 spe-9 Run 16/']     #15
 md_list = [
-    {'W':'/mnt/iscopearray/Zhang_William/2016.02.16 spe-9 Run 9/experiment_metadata.json','D':'/mnt/bulkhelium/Sinha_Drew/20160216_spe9Acquisition/experiment_metadata.json'},   #0
+    {'W':'/mnt/iscopearray/zhangw/2016.02.16 spe-9 Run 9/experiment_metadata.json','D':'/mnt/scopearray/Sinha_Drew/20160216_spe9Acquisition/experiment_metadata.json'},   #0
     {'':''},    #1
     {'':''},    #2
     {'':''},    #3
     {'':''},    #4
     {'':''},    #5
     {'':''},    #6
-    {'W':'/mnt/iscopearray/Zhang_William/2016.02.29 spe-9 Run 12A/experiment_metadata.json', 'D':'/mnt/bulkhelium/Sinha_Drew/20160229_spe9Acquisition_DevVarA/experiment_metadata.json'},    #7
-    {'W':'/mnt/iscopearray/Zhang_William/2016.02.29 spe-9 Run 12B/experiment_metadata.json','D':'/mnt/bulkhelium/Sinha_Drew/20160229_spe9Acquisition_DevVarB/experiment_metadata.json'},    #8
+    {'W':'/mnt/iscopearray/zhangw/2016.02.29 spe-9 Run 12A/experiment_metadata.json', 'D':'/mnt/scopearray/Sinha_Drew/20160229_spe9Acquisition_DevVarA/experiment_metadata.json'},    #7
+    {'W':'/mnt/iscopearray/zhangw/2016.02.29 spe-9 Run 12B/experiment_metadata.json','D':'/mnt/scopearray/Sinha_Drew/20160229_spe9Acquisition_DevVarB/experiment_metadata.json'},    #8
     {'':''},    #9
-    {'W':'/mnt/iscopearray/Zhang_William/2016.03.04 spe-9 Run 13B/experiment_metadata.json', 'D':'/mnt/bulkhelium/Sinha_Drew/20160304_spe9Acquisition_DevVarB/experiment_metadata.json'},    #10
-    {'W':'/mnt/iscopearray/Zhang_William/2016.03.04 spe-9 Run 13B/experiment_metadata.json', 'D':'/mnt/bulkhelium/Sinha_Drew/20160304_spe9Acquisition_DevVarC/experiment_metadata.json'},    #11
+    {'W':'/mnt/iscopearray/zhangw/2016.03.04 spe-9 Run 13B/experiment_metadata.json', 'D':'/mnt/scopearray/Sinha_Drew/20160304_spe9Acquisition_DevVarB/experiment_metadata.json'},    #10
+    {'W':'/mnt/iscopearray/zhangw/2016.03.04 spe-9 Run 13B/experiment_metadata.json', 'D':'/mnt/scopearray/Sinha_Drew/20160304_spe9Acquisition_DevVarC/experiment_metadata.json'},    #11
     {'':''},    #12
     {'':''},    #13
     {'':''},    #14
