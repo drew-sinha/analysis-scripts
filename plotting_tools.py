139eda7919e6a86c0bf1e104dbd534c1c8dcb67d
DS <>
2017-08-14 11:19:20 -0500

    (plotting_tools) Cleaned up; clean_plot uses kws and no longer suffers from weird tick problems (finds hidden ticks automatically); helper for saving cleaned figs (save_cleaned_fig)

--- plotting_tools.py
+++ plotting_tools.py
@@ -114,74 +114,16 @@ qual_colors = np.array([[0,0,0],    # Switched the 2nd and 9th colors
     #~ ax_h[1].set_xlabel('Time to death (d)')
     #~ ax_h[1].set_ylabel('Frequency')
     #~ ax_h[1].set_title('Mean+/-STD: {:.2f}+/-{:.2f}d\nMedian:{}d'.format(np.mean(lifespan),np.std(lifespan),np.median(lifespan)))
-    
-#~ def plot_lifespan(ann_fps, expt_mds, annotation_prefix_list = [], bad_worm_kws=[],debug_mode=False,plot_mode='both',hist_mode='kde'):
-    #~ def draw_hist(data, my_ax, hist_mode = 'kde'):
-        #~ if hist_mode is 'kde':
-            #~ interval_supp,interval_data,interval_obj = zplib.scalar_stats.kde.kd_distribution(data)
-            #~ my_ax.plot(interval_supp,interval_data)
-        #~ elif hist_mode is 'bar':
-            #~ my_ax.hist(data)
-        #~ return my_ax
-        
-    #~ if annotation_prefix_list == []:
-        #~ my_ann_files = [annotation_file.AnnotationFile(ann_fp) for ann_fp in ann_fps]
-    #~ else:
-        #~ my_ann_files = [annotation_file.AnnotationFile(ann_fp,annotation_prefix=prefix) for (ann_fp,prefix) in zip(ann_fps,annotation_prefix_list)]
-    
-    #~ timestamped_data = {}
-    #~ [timestamped_data.setdefault(expt_key,np.array([])) for expt_key in list(my_ann_files[0].data.keys())]
-    #~ for [expt_md_fp, ann_file] in zip(expt_mds, my_ann_files):
-        #~ if debug_mode: print(expt_md_fp)
-        #~ if type(expt_mds[0]) == str:    # Simple - one md file
-            #~ ann_file_data = ann_file.data_as_timestamps_simple(expt_md_fp)
-        #~ if type(expt_mds[0]) == dict:   # Need to link multiple md files
-            #~ ann_file_data = ann_file.data_as_timestamps(expt_md_fp)
-            
-        #~ for expt_key in timestamped_data.keys():
-            #~ timestamped_data[expt_key] = np.append(timestamped_data[expt_key],ann_file_data[expt_key])
-    
-    #~ viable_worm = (timestamped_data['Hatch']!=-1) \
-        #~ & (timestamped_data['Death']!=-1) \
-        #~ & np.array([not any([kw in note for kw in bad_worm_kws]) for note in timestamped_data['Notes']])
-    #~ print(timestamped_data['Worm'][viable_worm])
-    
-    #~ lifespan = (timestamped_data['Death']-timestamped_data['Hatch'])[viable_worm]/(3600*24) # Days
-    #~ sorted_ls = sorted(lifespan)
-    #~ prop_alive = 1 - (np.arange(start=0,stop=np.size(lifespan)))/np.size(lifespan)
-    
-    #~ plt.show()
-    #~ plt.ion()
-    
-    #~ plt.gcf().clf()
-    #~ if plot_mode is 'both':
-        #~ fig_h, ax_h = plt.subplots(2,1,sharex=True)
-        #~ ax_h[0].plot(np.append([0],sorted_ls),np.append([1],prop_alive))
-        #~ ax_h[0].set_xlabel('Time since expt. start (d)')
-        #~ ax_h[0].set_ylabel('Proportion alive')
-        #~ ax_h[0].set_title('Survival curve - n = {}'.format(np.size(lifespan)))
-        
-        #~ ax_h[1].hist(lifespan)
-        #~ ax_h[1].set_xlabel('Time to death (d)')
-        #~ ax_h[1].set_ylabel('Frequency')
-        #~ ax_h[1].set_title('Mean+/-STD: {:.2f}+/-{:.2f}d\nMedian:{}d'.format(np.mean(lifespan),np.std(lifespan),np.median(lifespan)))
-    #~ elif plot_mode is 'lifespan':
-        #~ fig_h,ax_h = plt.subplots(1,1)
-        #~ draw_hist(lifespan, ax_h)
-        #~ ax_h.set_xlabel('Time to death (d)')
-        #~ ax_h.set_ylabel('Frequency')
-        #~ ax_h.set_title('Mean+/-STD: {:.2f}+/-{:.2f}d\nMedian:{}d'.format(np.mean(lifespan),np.std(lifespan),np.median(lifespan)))
-    #~ else:
-        #~ fig_h,ax_h = plt.subplots(1,1)
-        #~ ax_h.plot(np.append([0],sorted_ls),np.append([1],prop_alive))
-        #~ ax_h.set_xlabel('Time since expt. start (d)')
-        #~ ax_h.set_ylabel('Proportion alive')
-        #~ ax_h.set_title('Survival curve - n = {}'.format(np.size(lifespan)))
-    
-    #~ return fig_h, ax_h
 
-
-def clean_plot(my_plot,make_labels=False,suppress_ticklabels=False, cleaning_mode=None):
+def clean_plot(my_plot, cleaning_mode=None,**kws):
+    if cleaning_mode is None:
+        make_labels = kws.get('make_labels',False)
+        suppress_ticklabels = kws.get('suppress_ticklabels',False)
+    spines_off = kws.get('spines_off',['right','top'])
+    num_xticks = kws.get('num_xticks',0)
+    num_yticks = kws.get('num_yticks',0)
+    square_aspect = kws.get('square_aspect',False)
+    
     if cleaning_mode == 'visualize':
         make_labels=False
         suppress_ticklabels = False
@@ -192,25 +134,40 @@ def clean_plot(my_plot,make_labels=False,suppress_ticklabels=False, cleaning_mod
         make_labels = True
         suppress_ticklabels = False
     
-    my_plot.spines['right'].set_visible(False)
-    my_plot.spines['top'].set_visible(False)
+    #~ my_plot.spines['right'].set_visible(False)
+    #~ my_plot.spines['top'].set_visible(False)
+    [my_plot.spines[spine_line].set_visible(False) for spine_line in spines_off]
     
     my_plot.tick_params(axis='both',which='both', top='off', bottom='off', left='off', right='off')
     
-    #if not suppress_ticklabels:
-    full_xticks = my_plot.get_xticks().copy()
-    full_xticklabels = my_plot.get_xticklabels().copy()
-    if full_xticks[0]*full_xticks[-1] < 0:
-        my_plot.set_xticks([full_xticks[0], 0, full_xticks[-1]])
-    else:
-        my_plot.set_xticks([full_xticks[0], full_xticks[-1]])
+    if len(my_plot.get_xticks())>0:
+        full_xticks = my_plot.get_xticks().copy()
+        full_xticklabels = my_plot.get_xticklabels().copy()
+        xlim = my_plot.get_xlim()
+        first_xtick = full_xticks[full_xticks>=xlim[0]][0]  # Sometimes there are hidden ticks not visible within limits...
+        last_xtick = full_xticks[full_xticks<=xlim[1]][-1]
+        if first_xtick*last_xtick < 0:
+            my_plot.set_xticks([first_xtick, 0, last_xtick])
+        else:
+            my_plot.set_xticks([first_xtick, last_xtick])
+        if num_xticks > 0:
+            my_plot.set_xticks(np.linspace(first_xtick, last_xtick, num_xticks))
+        my_plot.xaxis.set_ticks_position('none')
     
-    full_yticks = my_plot.get_yticks().copy()
-    full_yticklabels = my_plot.get_yticklabels().copy()
-    if full_yticks[0]*full_yticks[-1] < 0:
-        my_plot.set_yticks([full_yticks[0], 0, full_yticks[-1]])
-    else:
-        my_plot.set_yticks([full_yticks[0], full_yticks[-1]])
+    if len(my_plot.get_yticks())>0:
+        full_yticks = my_plot.get_yticks().copy()
+        full_yticklabels = my_plot.get_yticklabels().copy()
+        ylim = my_plot.get_ylim()
+        first_ytick = full_yticks[full_yticks>=ylim[0]][0]
+        last_ytick = full_yticks[full_yticks<=ylim[1]][-1]
+        if first_ytick*last_ytick < 0:
+            my_plot.set_yticks([first_ytick, 0, last_ytick])
+        else:
+            my_plot.set_yticks([first_ytick, last_ytick])
+        if num_yticks > 0:
+            my_plot.set_yticks(np.linspace(first_ytick, last_ytick, num_yticks))
+        my_plot.yaxis.set_ticks_position('none')
+        
     #else:
     if suppress_ticklabels:
         my_plot.set_xticks([])
@@ -220,6 +177,20 @@ def clean_plot(my_plot,make_labels=False,suppress_ticklabels=False, cleaning_mod
         my_plot.set_xlabel('')
         my_plot.set_ylabel('')
         my_plot.set_title('')
+    
+    if square_aspect:
+        x0,x1 = my_plot.get_xlim()
+        y0,y1 = my_plot.get_ylim()
+        my_plot.set_aspect(abs(x1-x0)/abs(y1-y0))
+
+def save_cleaned_fig(fig_h,ax_h,out_fn,**kws):
+    if 'cleaning_mode' not in kws:
+        kws['cleaning_mode'] = 'verbose'
+    if type(ax_h) in [list, np.ndarray]:
+        [clean_plot(my_ax,**kws) for my_ax in flatten_list(ax_h)]
+    else:
+        clean_plot(ax_h,**kws)
+    fig_h.savefig(out_fn)
 
 def force_same_plot_attributes(my_axes, *args):
     for attr in args:
