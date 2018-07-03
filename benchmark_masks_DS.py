6c4db1566971d3b04233785eec6cba889944acbd
drew-sinha <>
2018-07-03 15:41:36 -0500

    Extra tidbits in benchmarking for overlaying masks

--- benchmark_masks_DS.py
+++ benchmark_masks_DS.py
@@ -21,3 +21,37 @@ def benchmark_masks_DS(expt_dir):
                 
                 ious[image_key] = (mask_image & manual_mask).sum() / (mask_image | manual_mask).sum()
     return ious
+
+def overlay_masks(rw, position_directory):
+    position_directory = pathlib.Path(position_directory)
+    expt_dir = position_directory.parent
+    position_annotations = load_data.read_annotations(expt_dir)[position_directory.name]
+    
+    files_to_load = []
+    page_names = []
+    global_positions, timepoint_annotations = position_annotations
+    for timepoint, timepoint_data in timepoint_annotations.items():
+        image_key = position_directory.name + '_' + timepoint
+        image = freeimage.read(str(position_directory / (timepoint + ' bf.png')))
+        mask_file = expt_dir / 'derived_data' / 'mask' / position_directory.name / (timepoint + ' bf.png')
+        if mask_file.exists():
+            mask_image = freeimage.read(str(expt_dir / 'derived_data' / 'mask' / position_directory.name / (timepoint + ' bf.png'))) > 0
+            files_to_load.append([image, mask_image])
+        else:
+            files_to_load.append([image])
+    rw.flipbook_pages = files_to_load
+
+
+'''
+# Example code for plotting a distribution of ious
+from zplib.scalar_stats import kde
+import matplotlib.pyplot as plt
+
+fig_h, ax_h = plt.subplots()
+ax_h.set_title('Accuracy of LRR pipeline')
+iou_vals = np.array([v for k,v in ious.items()])
+ax_h.plot(*kde.kd_distribution(iou_vals)[:-1], color = 'k')
+ax_h.set_xlim([0,1])
+ax_h.set_xlabel('IoU')
+ax_h.set_ylabel('Density')
+'''
