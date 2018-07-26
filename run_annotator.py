4034a2f881f3de6c2065d620b67d5d12db068ac5
drew-sinha <>
2018-07-26 00:49:16 -0500

    run_annotator flexibility for larval annotations; beta

--- run_annotator.py
+++ run_annotator.py
@@ -8,25 +8,25 @@ from elegant import load_data
 from elegant.gui import experiment_annotator, stage_field
 
 def check_stage_annotations(annotations, stages):
-    """Check that a set of annotations are complete 
-        
+    """Check that a set of annotations are complete
+
         Parameters
             annotations - An OrderedDict mapping position names to corresponding
                 annotations (returned by load_data.read_annotations)
             stages - A iterable containing the stages that should be annotated
                 for this experiment (e.g. could be ('larva','adult','dead')
-                for a complete experiment, but only ('larva', 'adult') for 
+                for a complete experiment, but only ('larva', 'adult') for
                 an ongoing experiment)
         Returns
             bad_positions - a list of positions with incomplete annotations
     """
-    
+
     # Create a suitable function to use with filter_positions using a closure
     def select_by_stage_annotation(position_name,position_annotations, timepoint_annotations):
         stage_annotations = [timepoint_annotation.get('stage','')
             for timepoint_annotation in timepoint_annotations.values()]
         return all([stage in stage_annotations for stage in stages])
-    
+
     return load_data.filter_annotations(
         annotations,
         select_by_stage_annotation) # Get positions whose stages are not all annotated
@@ -57,26 +57,34 @@ def check_for_alive(expt_dir):
     dead_annotations = check_stage_annotations(good_annotations, ['dead'])
 
     print(f'{len(good_annotations)-len(dead_annotations)}/{len(good_annotations)} still alive')
-    
+
     return set(good_annotations.keys()).difference(set(dead_annotations.keys()))
 
 if __name__ == "__main__":
-    '''Call as python run_annotator.py EXPT_DIR'''
-    
+    '''Call as python run_annotator.py EXPT_DIR [MODE]'''
+
     try:
         rw
     except NameError:
         rw = ris_widget.RisWidget()
         rw.show()
-    
+
     if hasattr(rw, 'annotator'):
         rw.annotator.close()
         del(rw.annotator)
-    
+
     sf = stage_field.StageField()
-    
+
     expt_dir = pathlib.Path(sys.argv[1])
     expt_pos = load_data.scan_experiment_dir(expt_dir)
-    
+
+    if len(sys.argv[2]) == 2 or sys.argv[2] == 'adult':
+        sf = stage_field.StageField()
+    elif sys.argv[2] == 'larval':
+        stages = ['egg','L1','L2','L3','L4','young_adult','adult','dead']
+        transitions = ['hatch','L1m','L2m','L3m','L4m','egg-laying','death']
+        shortcuts = ['h','1','2','3','4','v','d']
+        sf = stage_field.StageField(stages=stages,transitions=transitions,shortcuts=shortcuts)
+
     ea = experiment_annotator.ExperimentAnnotator(rw, expt_dir.parts[-1],
         expt_pos, [sf])
