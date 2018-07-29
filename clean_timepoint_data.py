4621cd0ea32e707c040614281124e6129753fd0b
drew-sinha <>
2018-07-29 16:36:16 -0500

    remove_offending_tp stable; cleaned up and doc'd

--- clean_timepoint_data.py
+++ clean_timepoint_data.py
@@ -7,16 +7,18 @@ import numpy as np
 from zplib import datafile
 from elegant import load_data
 
-def remove_offending_tp(expt_path,timept_str,dry_run=False):
-    '''
-        expt_path - str/pathlib.Path pointing to expt. dir
-        timept_str - str in format yyyymmdd-thhmm
-        dry_run - Toggles taking action (if False, will not delete but will verbosely specify where offending files are found
+def remove_offending_tp(experiment_root,timept_str,dry_run=False):
+    '''Removes all of the files/entries for a given timepoint and backs them up elsewhere
+
+    Parameters
+        experiment_root - str/pathlib.Path pointing to experiment directory
+        timept_str - str with standard format for timepoint acquisition (YYYY-MM-DDtHHMM)
+        dry_run - bool flag that toggles taking action (if False, only specifies when offending files are found)
     '''
 
-    if type(expt_path) is str: expt_path = pathlib.Path(expt_path)
+    if type(experiment_root) is str: experiment_root = pathlib.Path(experiment_root)
 
-    for sub_dir in expt_path.iterdir():
+    for sub_dir in sorted(experiment_root.iterdir()):
         if sub_dir.is_dir():
 
             # Move bad files
@@ -25,38 +27,36 @@ def remove_offending_tp(expt_path,timept_str,dry_run=False):
                 print('Found offending files in: '+str(sub_dir))
 
                 if not dry_run:
-                    if not (sub_dir/'offending_files').exists(): (sub_dir/'offending_files').mkdir(mode=755)
-                    [im_file.rename(im_file.parent/'offending_files'/im_file.parts[-1]) for im_file in offending_files]
+                    if not (sub_dir/'offending_files').exists(): (sub_dir/'offending_files').mkdir(mode=775)
+                    [im_file.rename(im_file.parent/'offending_files'/im_file.name) for im_file in offending_files]
 
             # Check if position metadata is bad and handle
-            if sub_dir.parts[-1] != 'calibrations':
+            if sub_dir.name.isnumeric():
                 md_file = (sub_dir/'position_metadata.json')
-                if not md_file.exists(): print('No metadata file found at:'+str(sub_dir))
-                else:
-                    with md_file.open() as md_fp:
-                        pos_md = json.load(md_fp)
-                    has_bad_md_tmpt = any([tmpt['timepoints'] == timept_str for tmpt in pos_md])
-                    if has_bad_md_tmpt:
-                        print('Found offending entry in position_metadata in: '+str(sub_dir))
-                        if not dry_run:
-                            if not (sub_dir/'offending_files').exists(): (sub_dir/'offending_files').mkdir(mode=755)
-                            md_file.rename(md_file.parent/'offending_files'/'position_metadata_old.json')     # Backup old
-                            pos_md = [tp_data for tp_data in pos_md if tp_data['timepoints'] != timept_str]
-                            with (sub_dir/'position_metadata.json').open('w') as md_fp:
-                                encode_legible_to_file(pos_md,md_fp)   # Write out new position_metadata
+                with md_file.open() as md_fp:
+                    pos_md = json.load(md_fp)
+                has_bad_md_tmpt = any([tmpt['timepoints'] == timept_str for tmpt in pos_md])
+                if has_bad_md_tmpt:
+                    print('Found offending entry in position_metadata in: '+str(sub_dir))
+                    if not dry_run:
+                        if not (sub_dir/'offending_files').exists(): (sub_dir/'offending_files').mkdir(mode=775)
+                        md_file.rename(md_file.parent/'offending_files'/'position_metadata_old.json')     # Backup old
+                        pos_md = [tp_data for tp_data in pos_md if tp_data['timepoints'] != timept_str]
+                        datafile.json_encode_atomic_legible_to_file(pos_md,sub_dir/'position_metadata.json')   # Write out new position_metadata
 
-            if (expt_dir / 'annotations' / sub_dir.name).exists():
-                position_annotations,timepoint_annotations = load_data.read_annotation_file(expt_dir / 'annotations' / sub_dir.name)
+            position_annotation_file = experiment_root / 'annotations' / f'{sub_dir.name}.pickle'
+            if position_annotation_file.exists():
+                general_annotations,timepoint_annotations = load_data.read_annotation_file(position_annotation_file)
 
                 if timept_str in timepoint_annotations:
-                    print(f'Found annotation for position {sub_dir.name}')
+                    print(f'Found offending entry in annotation for position {sub_dir.name}')
                     if not dry_run:
-                        timepoint_annotations.pop(timept_str)
-                        load_data.write_annotation_file(expt_dir / 'annotations' / sub_dir.name,
-                            position_annotations, timepoint_annotations)
+                        del timepoint_annotations[timept_str]
+                        load_data.write_annotation_file(position_annotation_file,
+                            general_annotations, timepoint_annotations)
 
 
-    md_file = (expt_path/'experiment_metadata.json')
+    md_file = (experiment_root/'experiment_metadata.json')
     with md_file.open() as md_fp:
         expt_md = json.load(md_fp)
 
@@ -68,8 +68,7 @@ def remove_offending_tp(expt_path,timept_str,dry_run=False):
         expt_md['brightfield_metering']={key:val for key,val in expt_md['brightfield_metering'] if key != timept_str}
 
         md_file.rename(md_file.parent/'experiment_metadata_old.json') #Backup
-        with (expt_path/'experiment_metadata.json').open('w') as md_fp:  # Write out new
-            datafile.encode_atomic_legible_to_file(expt_md,md_fp)
+        datafile.json_encode_atomic_legible_to_file(expt_md,experiment_root/'experiment_metadata.json')
     except:  # Offending timepoint didn't make it into metadata
         pass
 
@@ -113,7 +112,8 @@ def clean_dead_timepoints(experiment_root, postmortem_time, delete_excluded=Fals
                 time_since_death = (_extract_datetime_fromstr(timepoint) - _extract_datetime_fromstr(death_timepoint))/3600
                 if time_since_death > postmortem_time:
                     timepoints_to_delete.append(timepoint)
-        [del timepoint_annotations[dead_timepoint] for dead_timepoint in timepoints_to_delete]
+        for dead_timepoint in timepoints_to_delete:
+            del timepoint_annotations[dead_timepoint]
         load_data.write_annotation_file(experiment_root /'annotations' / f'{position}.tsv', general_annotations, timepoint_annotations)
 
 def _extract_datetime_fromstr(time_str):
