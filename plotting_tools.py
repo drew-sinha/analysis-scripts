9bbd86ae64ff47b7c9d7d2f400808ad5f2a53d13
drew-sinha <drew.sinha@wustl.edu>
2018-08-28 18:37:52 -0500

    plotting_tools bugfix after utilities reorganization

--- plotting_tools.py
+++ plotting_tools.py
@@ -4,7 +4,7 @@ import os
 import zplib.scalar_stats.kde
 import skimage.color
 
-import utilties
+from utilities import utilities
 
 #~ import annotation_file
 
@@ -35,11 +35,11 @@ qual_colors = np.array([[0,0,0],    # Switched the 2nd and 9th colors
     [211,66,126],
     [86,165,116],
     [203,81,54]])/255
-    
+
 def build_gradient_palette(base_color, num_colors):
     '''
         Build a gradient color palette using a provided color as the base and modifying the intensity in LAB space
-        
+
         base_color - 3-item list or np.array containing the base color (no alpha channel)
         num_colors - (int) Number of desired colors
     '''
@@ -47,19 +47,19 @@ def build_gradient_palette(base_color, num_colors):
     # Handle black (really want white here...)
     if (np.array(base_color)==np.array([0,0,0])).all():
         base_color = np.array([1,1,1])*.99 # can't do pure white for stability
-    
+
     base_color_lab = skimage.color.rgb2lab(np.array([[base_color]]))
-    
+
     return ([skimage.color.lab2rgb(
         np.array([[[
             100-(base_color_lab[0,0,0]*i/num_colors),
             base_color_lab[0,0,1],
-            base_color_lab[0,0,2]]]]))[0,0,:] 
+            base_color_lab[0,0,2]]]]))[0,0,:]
         for i in range(num_colors+1)][1:])
 
 #~ def quick_plot_dev(ann_fps, expt_mds,bad_worm_kws=[]):
-    #~ my_ann_files = [annotation_file.AnnotationFile(ann_fp) for ann_fp in ann_fps]  
-    
+    #~ my_ann_files = [annotation_file.AnnotationFile(ann_fp) for ann_fp in ann_fps]
+
     #~ timestamped_data = {}
     #~ [timestamped_data.setdefault(expt_key,np.array([])) for expt_key in my_ann_files[0].get_data_keys()]
     #~ for [expt_md_fp, ann_file] in zip(expt_mds, my_ann_files):
@@ -67,21 +67,21 @@ def build_gradient_palette(base_color, num_colors):
         #~ for expt_key in timestamped_data.keys():
             #~ timestamped_data[expt_key] = np.append(timestamped_data[expt_key],ann_file_data[expt_key])
     #~ print(timestamped_data)
-    
+
     #~ viable_worm = (timestamped_data['Hatch']!=-1) \
         #~ & np.array([not any([kw in note for kw in bad_worm_kws]) for note in timestamped_data['Notes']])
     #~ hatched_on_corral = timestamped_data['Hatch'] != 0
-    
+
     #~ L1_durations = (timestamped_data['L1 ecdysis']-timestamped_data['Hatch'])[viable_worm & hatched_on_corral]/3600
     #~ L2_durations = (timestamped_data['L2 ecdysis']-timestamped_data['L1 ecdysis'])[viable_worm]/3600
     #~ L3_durations = (timestamped_data['L3 ecdysis']-timestamped_data['L2 ecdysis'])[viable_worm]/3600
     #~ L4_durations = (timestamped_data['L4 ecdysis']-timestamped_data['L3 ecdysis'])[viable_worm]/3600
     #~ larval_durations = (timestamped_data['L4 ecdysis']-timestamped_data['Hatch'])[viable_worm & hatched_on_corral]/3600
     #~ print(L1_durations)
-    
+
     #~ plt.show()
     #~ plt.ion()
-    
+
     #~ plt.gcf().clf()
     #~ fig_h,ax_h = plt.subplots(5,1,sharex=True)
 
@@ -104,8 +104,8 @@ def build_gradient_palette(base_color, num_colors):
     #~ ax_h[4].set_title('Maturity (n={}) - Mean: {}, Std:{}'.format(np.size(larval_durations),np.mean(larval_durations),np.std(larval_durations)))
 
 #~ def quick_plot_lifespan(ann_fps, expt_mds, bad_worm_kws=[],debug_mode=False):
-    #~ my_ann_files = [annotation_file.AnnotationFile(ann_fp) for ann_fp in ann_fps]  
-    
+    #~ my_ann_files = [annotation_file.AnnotationFile(ann_fp) for ann_fp in ann_fps]
+
     #~ timestamped_data = {}
     #~ [timestamped_data.setdefault(expt_key,np.array([])) for expt_key in my_ann_files[0].get_data_keys()]
     #~ for [expt_md_fp, ann_file] in zip(expt_mds, my_ann_files):
@@ -113,26 +113,26 @@ def build_gradient_palette(base_color, num_colors):
         #~ ann_file_data = ann_file.data_as_timestamps_simple(expt_md_fp)
         #~ for expt_key in timestamped_data.keys():
             #~ timestamped_data[expt_key] = np.append(timestamped_data[expt_key],ann_file_data[expt_key])
-    
+
     #~ viable_worm = (timestamped_data['Hatch']!=-1) \
         #~ & (timestamped_data['Death']!=-1) \
         #~ & np.array([not any([kw in note for kw in bad_worm_kws]) for note in timestamped_data['Notes']])
     #~ print(timestamped_data['Worm'][viable_worm])
-    
+
     #~ lifespan = (timestamped_data['Death']-timestamped_data['Hatch'])[viable_worm]/(3600*24) # Days
     #~ sorted_ls = sorted(lifespan)
     #~ prop_alive = 1 - (np.arange(start=0,stop=np.size(lifespan)))/np.size(lifespan)
-    
+
     #~ plt.show()
     #~ plt.ion()
-    
+
     #~ plt.gcf().clf()
     #~ fig_h, ax_h = plt.subplots(2,1,sharex=True)
     #~ ax_h[0].plot(np.append([0],sorted_ls),np.append([1],prop_alive))
     #~ ax_h[0].set_xlabel('Time since expt. start (d)')
     #~ ax_h[0].set_ylabel('Proportion alive')
     #~ ax_h[0].set_title('Survival curve - n = {}'.format(np.size(lifespan)))
-    
+
     #~ ax_h[1].hist(lifespan)
     #~ ax_h[1].set_xlabel('Time to death (d)')
     #~ ax_h[1].set_ylabel('Frequency')
@@ -148,7 +148,7 @@ def clean_plot(my_plot, cleaning_mode=None,**kws):
     square_aspect = kws.get('square_aspect',False)
     ylim = kws.get('ylim', None)
     xlim = kws.get('xlim', None)
-    
+
     if cleaning_mode == 'visualize':
         make_labels=False
         suppress_ticklabels = False
@@ -158,16 +158,16 @@ def clean_plot(my_plot, cleaning_mode=None,**kws):
     elif cleaning_mode == 'verbose':
         make_labels = True
         suppress_ticklabels = False
-    
+
     [my_plot.spines[spine_line].set_visible(False) for spine_line in spines_off]
-    
+
     my_plot.tick_params(axis='both',which='both', top='off', bottom='off', left='off', right='off')
-    
+
     if ylim is not None:
         my_plot.set_ylim(ylim)
     if xlim is not None:
         my_plot.set_xlim(xlim)
-    
+
     if num_xticks is not None and len(my_plot.get_xticks())>0:
         full_xticks = my_plot.get_xticks().copy()
         full_xticklabels = my_plot.get_xticklabels().copy()
@@ -181,7 +181,7 @@ def clean_plot(my_plot, cleaning_mode=None,**kws):
         if num_xticks > 0:
             my_plot.set_xticks(np.linspace(first_xtick, last_xtick, num_xticks))
         my_plot.xaxis.set_ticks_position('none')
-    
+
     if num_yticks is not None and len(my_plot.get_yticks())>0:
         full_yticks = my_plot.get_yticks().copy()
         full_yticklabels = my_plot.get_yticklabels().copy()
@@ -195,16 +195,16 @@ def clean_plot(my_plot, cleaning_mode=None,**kws):
         if num_yticks > 0:
             my_plot.set_yticks(np.linspace(first_ytick, last_ytick, num_yticks))
         my_plot.yaxis.set_ticks_position('none')
-        
+
     if suppress_ticklabels:
         my_plot.set_xticks([])
         my_plot.set_yticks([])
-    
+
     if not make_labels:
         my_plot.set_xlabel('')
         my_plot.set_ylabel('')
         my_plot.set_title('')
-    
+
     if square_aspect:
         x0,x1 = my_plot.get_xlim()
         y0,y1 = my_plot.get_ylim()
@@ -229,3 +229,4 @@ def force_same_plot_attributes(my_axes, *args):
             min_y = min([ax.get_ylim()[0] for ax in my_axes])
             max_y = max([ax.get_ylim()[1] for ax in my_axes])
             [ax.set_ylim([min_y,max_y]) for ax in my_axes]
+
