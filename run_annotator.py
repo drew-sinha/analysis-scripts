c9cb069d4ac2ae6db7ed0c51c9fb0b0db0efd40a
drew-sinha <drew.sinha@wustl.edu>
2018-08-28 14:51:14 -0500

    clean up; third argument not usable (functionality commented out)

--- run_annotator.py
+++ run_annotator.py
@@ -5,7 +5,7 @@ from collections import OrderedDict
 
 from ris_widget import ris_widget
 from elegant import load_data
-from elegant.gui import experiment_annotator, stage_field
+from elegant.gui import experiment_annotator, stage_field, pose_annotation
 
 def check_stage_annotations(annotations, stages):
     """Check that a set of annotations are complete
@@ -33,27 +33,21 @@ def check_stage_annotations(annotations, stages):
 
 def scan_late_life(expt_dir, datestr):
     annotations = load_data.read_annotations(expt_dir)
-    print(len(annotations))
-    good_annotations = load_data.filter_positions(annotations, filter_good)
-    print(len(good_annotations))
+    good_annotations = load_data.filter_positions(annotations, load_data.filter_excluded)
     def latelife_filter(position_name, timepoint_name):
         return timepoint_name > datestr and position_name in good_annotations
     return load_data.scan_experiment_dir(expt_dir, timepoint_filter=latelife_filter)
 
 def scan_latest(expt_dir):
     annotations = load_data.read_annotations(expt_dir)
-    print(len(annotations))
-    good_annotations = load_data.filter_positions(annotations, filter_good)
-    print(len(good_annotations))
+    good_annotations = load_data.filter_annotations(annotations, load_data.filter_excluded)
     def latelife_filter(position_name, timepoint_name):
         return position_name in good_annotations and timepoint_name > good_annotations[position_name][0]['__last_timepoint_annotated__']
     return load_data.scan_experiment_dir(expt_dir, timepoint_filter=latelife_filter)
 
 def check_for_alive(expt_dir):
     annotations = load_data.read_annotations(expt_dir)
-    print(len(annotations))
     good_annotations = load_data.filter_annotations(annotations, load_data.filter_excluded)
-    print(len(good_annotations))
     dead_annotations = check_stage_annotations(good_annotations, ['dead'])
 
     print(f'{len(good_annotations)-len(dead_annotations)}/{len(good_annotations)} still alive')
@@ -78,13 +72,16 @@ if __name__ == "__main__":
     expt_dir = pathlib.Path(sys.argv[1])
     expt_pos = load_data.scan_experiment_dir(expt_dir)
 
-    if len(sys.argv[2]) == 2 or sys.argv[2] == 'adult':
-        sf = stage_field.StageField()
-    elif sys.argv[2] == 'larval':
-        stages = ['egg','L1','L2','L3','L4','young_adult','adult','dead']
-        transitions = ['hatch','L1m','L2m','L3m','L4m','egg-laying','death']
-        shortcuts = ['h','1','2','3','4','v','d']
-        sf = stage_field.StageField(stages=stages,transitions=transitions,shortcuts=shortcuts)
+    # if (len(sys.argv) == 1) or (sys.argv[2] == 'adult'):
+    #     sf = stage_field.StageField()
+    # elif sys.argv[2] == 'larval':
+    #     stages = ['egg','L1','L2','L3','L4','young_adult','adult','dead']
+    #     transitions = ['hatch','L1m','L2m','L3m','L4m','egg-laying','death']
+    #     shortcuts = ['h','1','2','3','4','v','d']
+    #     sf = stage_field.StageField(stages=stages,transitions=transitions,shortcuts=shortcuts)
+
+    # width_estimator, width_pca_basis = pose_annotation.default_width_data(pixels_per_micron=1/1.3, experiment_temperature=20)
+    # pa = pose_annotation.PoseAnnotation(rw, mean_widths=width_estimator, width_pca_basis=width_pca_basis)
 
     ea = experiment_annotator.ExperimentAnnotator(rw, expt_dir.parts[-1],
         expt_pos, [sf])
