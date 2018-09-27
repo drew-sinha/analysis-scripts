afd5ff8d0f8caec8c044027d1d092c2f7a36eb90
drew-sinha <drew.sinha@wustl.edu>
2018-09-27 03:33:52 -0500

    Bugfixes for annotate_lawn; implemented and now using GMM-based lawn finding

--- lawn_measurements.py
+++ lawn_measurements.py
@@ -2,6 +2,8 @@ import numpy
 import freeimage
 from skimage import feature, filters
 from scipy.ndimage import morphology
+import scipy.ndimage.filters as ndi_filters
+from sklearn import mixture
 
 import zplib.image.mask as zpl_mask
 from elegant import load_data, process_images, worm_spline
@@ -17,6 +19,7 @@ class LawnMeasurements:
 
         experiment_root, position_name = position_root.parent, position_root.name
         lawn_mask = freeimage.read(experiment_root / 'derived_data' / 'lawn_masks' / f'{position_name}.png')
+        lawn_mask = lawn_mask.astype('bool')
 
         # Remove the animal from the lawn if possible.
         center_tck, width_tck = annotations.get('pose', (None, None))
@@ -38,16 +41,18 @@ class LawnMeasurements:
 def annotate_lawn(experiment_root, position, metadata, annotations):
     '''Position annotator used to find the lawn and associated metadata about it'''
 
+    print(f'Working on position {position}')
     position_root = experiment_root / position
     lawn_mask_root = experiment_root / 'derived_data' / 'lawn_masks'
+    lawn_mask_root.mkdir(parents=True, exist_ok=True)
 
     microns_per_pixel = process_images.microns_per_pixel(metadata['objective'],metadata['optocoupler'])
     num_images_for_lawn = 10
 
-    position_images = load_data.scan_experiment_dir(position_root.parent)[position_name]
+    position_images = load_data.scan_experiment_dir(experiment_root)[position]
     first_imagepaths = []
-    for timepoint, timepoint_images in list(position_images.items()):
-        if timepoint + ' bf.png' in timepoint_images:
+    for timepoint, timepoint_images in position_images.items():
+        if position_root / (timepoint + ' bf.png') in timepoint_images:
             first_imagepaths.append(str(position_root / (timepoint + ' bf.png')))
         if len(first_imagepaths) == num_images_for_lawn:
             break
@@ -57,11 +62,11 @@ def annotate_lawn(experiment_root, position, metadata, annotations):
         for image in first_images]
     median_first_images = numpy.median(first_images, axis=0)
 
-    individual_lawns = [edge_lawn_maker(image, metadata['optocoupler']) for image in first_images]
+    individual_lawns = [gmm_lawn_maker(image, metadata['optocoupler']) for image in first_images]
     lawn_mask = numpy.max(individual_lawns, axis=0)
     vignette_mask = process_images.vignette_mask(metadata['optocoupler'], lawn_mask.shape)
 
-    freeimage.write(lawn_mask, str(lawn_mask_root / f'{position}.png')) # Some better way to store this mask in the annotations?
+    freeimage.write(lawn_mask.astype('uint8')*255, str(lawn_mask_root / f'{position}.png')) # Some better way to store this mask in the annotations?
     annotations['lawn_area'] = lawn_mask.sum() * microns_per_pixel**2
 
 
@@ -74,10 +79,17 @@ def edge_lawn_maker(image, optocoupler):
 
     Returns:
         lawn mask as a bool ndarray
+
+    THIS ONE DOESN'T WORK AS ADVERTISED.... USE GMM_lawn_maker
     '''
 
-    filtered_image = filters.median(image)  # Preliminary median filtering
-    image_edges = feature.canny(image, sigma=0.02)
+    #filtered_image = filters.median(image)  # Preliminary median filtering
+    
+    #filtered_image=image
+    #filtered_image = filters.rank.mean(image, numpy.ones((3,3)))
+    import scipy.ndimage.filters as ndi_filters
+    filtered_image = ndi_filters.median_filter(image, size=(3,3), mode='constant')
+    image_edges = feature.canny(filtered_image, sigma=0.02)
     vignette_mask = process_images.vignette_mask(optocoupler, image.shape)
     image_edges[~vignette_mask] = False
 
@@ -88,6 +100,58 @@ def edge_lawn_maker(image, optocoupler):
         lawn_mask = morphology.binary_erosion(lawn_mask, iterations = 10)
     except:
         lawn_mask = numpy.zeros(lawn_mask.shape).astype('bool')
+
+    return lawn_mask
+
+def thr_lawn_maker(image, optocoupler):
+    '''
+       This one doesn't work particularly well either... use gmm_lawn_maker
+    '''
+    lawn_cutoff = numpy.percentile(image, 5)
+    lawn_mask = image < lawn_cutoff
+    vignette_mask = process_images.vignette_mask(optocoupler, image.shape)
+    lawn_mask[~(morphology.binary_erosion(vignette_mask, iterations=10))] = False
+    lawn_mask = morphology.binary_dilation(lawn_mask, iterations=3)
+    lawn_mask = morphology.binary_fill_holes(lawn_mask)
+    lawn_mask = zpl_mask.get_largest_object(lawn_mask)
+    lawn_mask = morphology.binary_erosion(lawn_mask, iterations=3)
+    lawn_mask = zpl_mask.get_largest_object(lawn_mask).astype('bool')
+    return lawn_mask
+
+def gmm_lawn_maker(image, optocoupler):
+    '''Find a lawn in an image use Gaussian mixture modeling
+
+    This lawn maker models an image (i.e. its pixel intensities) as as mixture
+        of two Gaussian densities. Each corresponds to either the background & lawn.
+
+    Parameters:
+        image - numpy ndarray of the image to find the lawn from
+        optocoupler - optocoupler magnification (as a float) used for the specified image
+
+    Returns:
+        lawn mask as a bool ndarray
+    '''
+    #scaled_image = process_images.pin_image_mode(image, optocoupler=optocoupler)
+    scaled_image = ndi_filters.median_filter(image, size=(3,3), mode='constant')
+    vignette_mask = process_images.vignette_mask(optocoupler, image.shape)
+    
+    img_data = scaled_image[vignette_mask]
+    img_hist = numpy.bincount(img_data.flatten())
+    img_hist = img_hist/img_hist.sum()
+
+    gmm = mixture.GaussianMixture(n_components=2)
+    gmm.fit(numpy.expand_dims(img_data,1))
+    
+    # Calculate boundary point for label classification as intensity threshold
+    gmm_support = numpy.linspace(0,2**16-1,2**16)
+    labels = gmm.predict(numpy.reshape(gmm_support, (-1,1)))
+    thr = numpy.argmax(numpy.abs(numpy.diff(labels)))
+    lawn_mask = (scaled_image < thr) & vignette_mask
+
+    lawn_mask = morphology.binary_erosion(lawn_mask, iterations=10)
+    lawn_mask = zpl_mask.get_largest_object(lawn_mask)
+    lawn_mask = morphology.binary_fill_holes(lawn_mask)
+    lawn_mask = morphology.binary_dilation(lawn_mask, iterations=10)
 
     return lawn_mask
 
