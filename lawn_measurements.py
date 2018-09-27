1c2c8f6d28df55c097f4e1cf3d775568b7e8b7de
drew-sinha <drew.sinha@wustl.edu>
2018-09-27 16:20:48 -0500

    Lawn model information improved; lawn intensity estimates now exclude some debris

--- lawn_measurements.py
+++ lawn_measurements.py
@@ -27,6 +27,10 @@ class LawnMeasurements:
         rescaled_image = process_images.pin_image_mode(timepoint_image, optocoupler=metadata['optocoupler'])
 
         lawn_mask = freeimage.read(experiment_root / 'derived_data' / 'lawn_masks' / f'{position_name}.png').astype('bool')
+        with (experiment_root / 'derived_data'/ 'lawn_models' / f'{position_name}.png').open('rb') as lm_file:
+            lawn_model = pickle.load(lm_file)
+
+        vignette_mask = process_images.vignette_mask(metadata['optocoupler'], timepoint_image.shape)
 
         # Remove the animal from the lawn if possible.
         center_tck, width_tck = annotations.get('pose', (None, None))
@@ -36,10 +40,16 @@ class LawnMeasurements:
             animal_mask = worm_spline.lab_frame_mask(center_tck, width_tck, timepoint_image.shape).astype('bool')
         lawn_mask = lawn_mask & ~animal_mask
 
-        vignette_mask = process_images.vignette_mask(metadata['optocoupler'], timepoint_image.shape)
+        # Remove debris from the mask (e.g. eggs, background junk in gel) defined by 3 stds below the lawn mean
+        lawn_mean = numpy.round(lawn_model['lawn_mean']).astype('int')
+        lower_bound = 1000 #numpy.percentile(rescaled_image[~vignette_mask], 95)
+        restricted_density = lawn_model['fitted_density'][lower_bound:lawn_center+1]
+        halfmax_pt = np.abs(restricted_density - restricted_density[-1]/2).argmin() + lower_bound
+        sigma = (lawn_center - halfmax_pt)/numpy.sqrt(2*numpy.log(2))
+        debris_mask = rescaled_image < lawn_center - 3/2*sigma
 
-        measures['summed_lawn_intensity'] = numpy.sum(rescaled_image[lawn_mask])
-        measures['median_lawn_intensity'] = numpy.median(rescaled_image[lawn_mask])
+        measures['summed_lawn_intensity'] = numpy.sum(rescaled_image[lawn_mask & ~debris_mask])
+        measures['median_lawn_intensity'] = numpy.median(rescaled_image[lawn_mask & ~debris_mask])
         measures['background_intensity'] = numpy.median(rescaled_image[~lawn_mask & vignette_mask])
 
         return [measures[feature_name] for feature_name in self.feature_names]
@@ -54,6 +64,9 @@ def annotate_lawn(experiment_root, position, metadata, annotations):
     position_root = experiment_root / position
     lawn_mask_root = experiment_root / 'derived_data' / 'lawn_masks'
     lawn_mask_root.mkdir(parents=True, exist_ok=True)
+
+    lawn_model_root = experiment_root / 'derived_data' / 'lawn_models'
+    lawn_model_root.mkdir(parents=True, exist_ok=True)
 
     microns_per_pixel = process_images.microns_per_pixel(metadata['objective'],metadata['optocoupler'])
     num_images_for_lawn = 10
@@ -74,13 +87,18 @@ def annotate_lawn(experiment_root, position, metadata, annotations):
     individual_lawns = [gmm_lawn_maker(image, metadata['optocoupler']) for image in first_images]
     lawn_mask = numpy.max(individual_lawns, axis=0)
 
-    median_lm, lawn_model = gmm_lawn_maker(median_first_images, metadata['optocoupler'], return_density=True)
+    median_lm, gmm_model = gmm_lawn_maker(median_first_images, metadata['optocoupler'], return_model=True)
 
     vignette_mask = process_images.vignette_mask(metadata['optocoupler'], lawn_mask.shape)
 
+    with (lawn_model_root / f'{position}.pickle').open('wb') as lm_file:
+        gmm_means = numpy.sort(gmm_model.means_.flatten())    # First value is lawn (since darker)
+        gmm_support = numpy.linspace(0,2**16-1,2**16)
+        gmm_density = numpy.exp(gmm_model.score_samples(gmm_support.reshape(-1,1)))
+        model_features = {'lawn_mean':gmm_centers[0], 'background_mean':gmm_centers[1], 'fitted_density':gmm_density}
+        pickle.dump(model_features, lm_file)
     freeimage.write(lawn_mask.astype('uint8')*255, str(lawn_mask_root / f'{position}.png')) # Some better way to store this mask in the annotations?
     annotations['lawn_area'] = lawn_mask.sum() * microns_per_pixel**2
-    annotations['lawn_model'] = lawn_model
 
 
 def edge_lawn_maker(image, optocoupler):
@@ -131,7 +149,7 @@ def thr_lawn_maker(image, optocoupler):
     lawn_mask = zpl_mask.get_largest_object(lawn_mask).astype('bool')
     return lawn_mask
 
-def gmm_lawn_maker(image, optocoupler, return_density=False):
+def gmm_lawn_maker(image, optocoupler, return_model=False):
     '''Find a lawn in an image use Gaussian mixture modeling (GMM)
 
     This lawn maker models an image (i.e. its pixel intensities) as as mixture
@@ -143,7 +161,7 @@ def gmm_lawn_maker(image, optocoupler, return_density=False):
 
     Returns:
         lawn mask as a bool ndarray
-        density corresponding to the fitted GMM
+        fitted GMM model
     '''
     #scaled_image = process_images.pin_image_mode(image, optocoupler=optocoupler)
     scaled_image = ndi_filters.median_filter(image, size=(3,3), mode='constant')
@@ -168,9 +186,8 @@ def gmm_lawn_maker(image, optocoupler, return_density=False):
     lawn_mask = morphology.binary_fill_holes(lawn_mask)
     lawn_mask = morphology.binary_dilation(lawn_mask, iterations=10)
 
-    if return_density:
-        gmm_density = numpy.exp(gmm.score_samples(gmm_support.reshape(-1,1)))
-        return lawn_mask, gmm_density
+    if return_model:
+        return lawn_mask, gmm
     else:
         return lawn_mask
 
