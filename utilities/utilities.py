9028e4b4efce28d25d3b339a2d1f0612b31e06be
drew-sinha <drew.sinha@wustl.edu>
2018-11-01 13:15:46 -0500

    Clean up utilities

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -30,23 +30,5 @@ def print_table(array_table, column_names=None, row_names=None, optional_formatt
     if optional_formatting is not None:
         array_table = [[optional_formatting.format(val) for val in row] for row in array_table]
 
-    # if row_names:
-    #     new_table = []
-    #     for row_name, array_row in zip(row_names, array_table):
-    #         base = [row_name]
-    #         base.extend(array_row)
-    #         new_table.append(base)
-    #     array_table = new_table
-    #
-    # if column_names:
-    #     column_names = [column_names]
-    #     if row_name:
-    #         temp = [None]
-    #         temp.extend(column_names)
-    #         column_names = temp
-    #
-    #     column_names.append(array_table)
-    #     array_table = column_names
-
     formatting_df = pandas.DataFrame(data=array_table, columns=column_names, index=row_names)
     display.display(formatting_df)
