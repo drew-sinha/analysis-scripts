a9e5184fa23853da999ba4404712a407d96d2697
drew-sinha <drew.sinha@wustl.edu>
2018-12-28 15:59:03 -0600

    run_annotator allows custom pose annotation; final cleanup

--- run_annotator.py
+++ run_annotator.py
@@ -1,26 +1,21 @@
 import pathlib
 import sys
 
-from collections import OrderedDict
-
-import numpy
-
 from ris_widget import ris_widget
-from elegant import load_data, worm_widths
+from elegant import load_data
 from elegant.gui import experiment_annotator, stage_field, pose_annotation
 
-import elegant_hacks
+import elegant_hacks, elegant_filters
 
 if __name__ == "__main__":
-    '''Call as python run_annotator.py EXPT_DIR [MODE]'''
-    import elegant_filters
+    '''Call as python run_annotator.py EXPT_DIR'''
     expt_dir = pathlib.Path(sys.argv[1])
 
     show_poses = False
     annotation_dir = 'annotations'
 
     timepoint_filters = [load_data.filter_excluded,elegant_filters.filter_live_animals, elegant_filters.filter_adult_timepoints] #, , elegant_filters.filter_after_timepoint('2018-10-10t1529')] #, , elegant_filters.filter_after_timepoint('2018-11-12t1200')  #[elegant_filters.filter_subsample_timepoints(expt_dir)]#elegant_filters.filter_range_before_stage(expt_dir, 3)] #load_data.filter_excluded] #[select_worms(expt_dir)] # [elegant_filters.filter_adult_dead_timepoints]#load_data.filter_excluded]
-    channels = ['bf'] #, 'gfp', 'autofluorescence'] #, 'green_yellow_excitation_autofluorescence']
+    channels = ['bf'] #, 'gfp', 'autofluorescence'] #, 'green_yellow_excitation_autofluorescence'] # First one is the one used to load poses when specified.
 
     try:
         rw
@@ -46,7 +41,11 @@ if __name__ == "__main__":
     annotation_fields.append(stage_field.StageField())
     if show_poses:
         metadata = load_data.read_metadata(expt_dir)
-        pa = pose_annotation.PoseAnnotation.from_experiment_metadata(metadata, rw)
+        if channels[0] == 'bf':
+            pose_name = 'pose'
+        else:
+            pose_name = f'{channels[0]} pose'
+        pa = pose_annotation.PoseAnnotation.from_experiment_metadata(metadata, rw, name=pose_name)
         annotation_fields.append(pa)
 
     ea = experiment_annotator.ExperimentAnnotator(rw, expt_dir.parts[-1],
