dc045ebfb35c6443e60807dc81d4e3a4684aee2b
drew-sinha <drew.sinha@wustl.edu>
2019-04-08 17:55:39 -0500

    Bugfix timepoint filter for non-updating annotations

--- run_annotator.py
+++ run_annotator.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     '''Call as python run_annotator.py EXPT_DIR'''
     expt_dir = pathlib.Path(sys.argv[1])
 
-    show_poses = False
+    show_poses = True
     show_masks = False
     readonly = False
     annotation_dir = 'annotations'
 
     # optional filters to eliminate relevant position/timepoints
-    timepoint_filters = [load_data.filter_excluded] #, elegant_filters.filter_live_animals, elegant_filters.filter_by_stage('adult'), elegant_filters.filter_after_timepoint('2019-02-02t1200')]
+    timepoint_filters = [load_data.filter_excluded] #, elegant_filters.select_worms(['15']), elegant_filters.filter_live_animals, elegant_filters.filter_by_stage('adult')] #, elegant_filters.filter_after_timepoint('2019-02-02t1200')]
     channels = ['bf'] #, 'gfp'] #, 'autofluorescence'] #, 'green_yellow_excitation_autofluorescence'] # First one is the one used to load poses when specified.
 
     try:
@@ -39,7 +39,7 @@ if __name__ == "__main__":
 
     expt_pos = load_data.scan_experiment_dir(expt_dir,
         channels=channels,
-        timepoint_filter = lambda position_name, timepoint_name: position_name in experiment_annotations and timepoint_name in experiment_annotations[position_name][1])
+        timepoint_filter = lambda position_name, timepoint_name: not experiment_annotations or (position_name in experiment_annotations and timepoint_name in experiment_annotations[position_name][1]))
     if show_masks:
         mask_root = expt_dir / 'derived_data' / 'mask'
         for position, position_images in expt_pos.items():
