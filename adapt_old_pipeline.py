59841efc8ce5fbe2bc8283ae86999d18eed53f70
drew-sinha <drew.sinha@wustl.edu>
2019-04-18 13:22:43 -0500

    Bugfixes; refactor annotation compilation to work with process_data

--- adapt_old_pipeline.py
+++ adapt_old_pipeline.py
@@ -6,12 +6,11 @@ import sys
 
 from elegant import process_data, load_data, process_experiment
 
-import elegant_hacks
+# import elegant_hacks
 
 previous_annotations = ['larva', 'adult', 'dead']
 
 def compile_annotations_from_tsv(experiment_root):
-    experiment_root = pathlib.Path(experiment_root.replace('\\ ', ' '))
     process_data.update_annotations(experiment_root)
 
     with (experiment_root / 'experiment_metadata.json').open('r') as mdata_file:
@@ -36,19 +35,20 @@ def compile_annotations_from_tsv(experiment_root):
     annotations = load_data.read_annotations(experiment_root)
     for position, (position_annotations, timepoint_annotations) in annotations.items():
         if 'DEAD' in annotation_data[position]['notes']:
+            # Sequentially add stage annotations in monotonic order and propagate.
             first_timepoint = list(timepoint_annotations.keys())[0]
             timepoint_annotations[first_timepoint]['stage'] = 'egg'
+            process_data.propagate_worm_stage(experiment_root, position, None, position_annotations, timepoint_annotations) # Pass None for position metadata since it isn't needed.
 
             for field in previous_annotations:
                 transition_timepoint = annotation_data[position][field]
                 timepoint_annotations[transition_timepoint]['stage'] = field
+                process_data.propagate_worm_stage(experiment_root, position, None, position_annotations, timepoint_annotations)
             position_annotations['exclude'] = False
         else:
             position_annotations['exclude'] = True
         position_annotations['notes'] = annotation_data[position]['notes']
     load_data.write_annotations(experiment_root, annotations)
-
-    elegant_hacks.propagate_stages(experiment_root) # Need this stage propagation since the stages aren't monotonically sequential
 
 def fill_empty_stages(experiment_root):
     '''
@@ -63,14 +63,15 @@ def fill_empty_stages(experiment_root):
     load_data.write_annotations(experiment_root, annotations)
 
 def move_great_lawn(experiment_root, remove_lawn=False):
-    experiment_root = pathlib.Path(experiment_root.replace('\\ ', ' '))
+    if type(experiment_root) is str:
+        experiment_root = pathlib.Path(experiment_root.replace('\\ ', ' '))
     (experiment_root / 'derived_data' / 'great_lawns').mkdir(parents=True,exist_ok=True)
     for position_root in [p.parent for p in experiment_root.glob('*/position_metadata.json')]:
         #print(position_root)
         if (position_root / 'great_lawn.png').exists():
             shutil.copyfile(str(position_root / 'great_lawn.png'), str(experiment_root / 'derived_data' / 'great_lawns' / f'{position_root.name}.png'))
-        if remove_lawn:
-            (position_root / 'great_lawn.png').unlink()
+            if remove_lawn:
+                (position_root / 'great_lawn.png').unlink()
 
 def prep_experiment_for_processing(experiment_root, nominal_temperature=None):
     move_great_lawn(experiment_root, remove_lawn=True)
@@ -79,8 +80,8 @@ def prep_experiment_for_processing(experiment_root, nominal_temperature=None):
         process_experiment.auto_update_metadata_file(experiment_root, nominal_temperature)
 
 if __name__ == "__main__":
-    experiment_root = pathlib.Path(experiment_root.replace('\\ ', ' '))
-    experiment_root = sys.argv[1]
+    experiment_root = pathlib.Path(sys.argv[1].replace('\\ ', ' '))
+    print(experiment_root)
     if len(sys.argv) > 2:
         try:
             assert sys.argv[2] in ['20','25']
