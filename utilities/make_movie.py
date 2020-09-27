988ea9b4845932226dd1a13e57f33301bfc9330e
Drew Sinha <drew.sinha@wustl.edu>
2020-09-27 00:32:16 -0500

    Clean up make movie and stable for image mode pinning

--- utilities/make_movie.py
+++ utilities/make_movie.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from zplib.image import write_movie
 import freeimage
+from elegant import process_images
 
 def yield_rgb(image_generator):
     for image in image_generator:
@@ -42,10 +43,16 @@ def double_image_layout(image_generator1, image_generator2):
         combined_image[image1.shape[0]:] = image2
         yield combined_image
 
+def pin_image(image_generator):
+    for image in image_generator:
+        yield process_images.pin_image_mode(image,optocoupler=1,new_mode=192)
 
 def make_experiment_movies(experiment_root, output_dir,
-    shrink_factor=4, framerate=5,glob_str='*bf.png',num_frames=-1,positions=None, **scale_params):
+    shrink_factor=4, framerate=5,glob_str='*bf.png',num_frames=-1,positions=None, pin_images_to_mode=False,
+     **scale_params):
     experiment_root = pathlib.Path(experiment_root)
+    output_dir = pathlib.Path(output_dir)
+
     if positions is None:
         position_roots = sorted(p.parent for p in experiment_root.glob('*/position_metadata.json'))
     else:
@@ -55,10 +62,13 @@ def make_experiment_movies(experiment_root, output_dir,
 
     for position_root in position_roots:
         image_paths = sorted(position_root.glob(glob_str))
+        print(image_paths)
         if num_frames != -1: image_paths = image_paths[:num_frames]
 
         image_generator = write_movie.generate_images_from_files(image_paths,**scale_params)
         image_generator = write_movie.shrink(image_generator, factor=shrink_factor, fast=True)
+        if pin_images_to_mode:
+            image_generator = pin_image(image_generator)
 
         output_file = output_dir / f'{position_root.name}.mp4'
         write_movie.write_movie(image_generator, output_file, framerate=framerate)
@@ -70,10 +80,10 @@ def make_experiment_movies(experiment_root, output_dir,
 # image_generator = write_movie.shrink(yield_rgb(image_generator), factor=4) #pyramid.pyr_down returning [x,y,1] shaped image leads to an assertion error.... Should the assertion be around?
 # write_movie.write_movie(image_generator, 'test.mp4',framerate=5)
 
-
-experiment_root = pathlib.Path('/mnt/purplearray/Sinha_Drew/20181109_spe-9_faster_acquisition/')
-shrink_factor = 8
-framerate = 10
-glob_str = '*bf_*.png'
-output_dir = experiment_root / 'derived_data' / 'movies' / f'shrink{shrink_factor}_framerate{framerate}_nopin'
-make_movie.make_experiment_movies(experiment_root, output_dir, shrink_factor=shrink_factor,framerate=framerate,glob_str=glob_str,positions=['17','30','35','58'])
+if __name__ == "__main__":
+    experiment_root = pathlib.Path('/mnt/purplearray/Sinha_Drew/20181109_spe-9_faster_acquisition/')
+    shrink_factor = 8
+    framerate = 10
+    glob_str = '*bf_*.png'
+    output_dir = experiment_root / 'derived_data' / 'movies' / f'shrink{shrink_factor}_framerate{framerate}_nopin'
+    make_movie.make_experiment_movies(experiment_root, output_dir, shrink_factor=shrink_factor,framerate=framerate,glob_str=glob_str,positions=['17','30','35','58'])
